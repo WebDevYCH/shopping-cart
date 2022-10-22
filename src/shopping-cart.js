7ab634711272cebfc4bd0d0649a066b65ffe62ae
webdevych <lee.smith870327@gmail.com>
2022-10-23 02:00:55 +0800

    commit by james

--- src/shopping-cart.js
+++ src/shopping-cart.js
@@ -6,16 +6,16 @@ import AppContext from "./context";
 export default () => {
     const context = React.useContext(AppContext);
     return (
-    <div>
-        <div className="shopping-cart">
-            Shopping Cart
-        </div>
-        {context.shoppingCart?.map((item, index) =>   
-            <div className="item" key={index}>{item.name}, {item.pricePerUnit} 
-                <button onClick={() => context.actions.removeProductFromCartAtIndex(index)}>Delete</button>
+        <div>
+            <div className="shopping-cart">
+                Shopping Cart
             </div>
-        )}
-    </div>
+            {context.shoppingCart?.map((item, index) =>   
+                <div className="item" key={index}>{item.name}, {item.pricePerUnit} 
+                    <button onClick={() => context.actions.removeProductFromCartAtIndex(index)}>Delete</button>
+                </div>
+            )}
+        </div>
     );
     
     
